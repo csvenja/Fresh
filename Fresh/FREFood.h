86980c452a6604a83957d732ab56db8399b2ed3c
csvenja <c.svenjax@gmail.com>
2014-05-04 16:59:05 -0700

    init food class

---
+++ Fresh/FREFood.h
@@ -0,0 +1,17 @@
+//
+//  FREFood.h
+//  Fresh
+//
+//  Created by Svenja on 2014-05-04.
+//  Copyright (c) 2014 Shan Cao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FREFood : NSObject
+
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSDate *productionDate;
+@property (nonatomic) NSNumber *freshDays;
+
+@end
---
+++ Fresh/FREFood.m
@@ -0,0 +1,13 @@
+//
+//  FREFood.m
+//  Fresh
+//
+//  Created by Svenja on 2014-05-04.
+//  Copyright (c) 2014 Shan Cao. All rights reserved.
+//
+
+#import "FREFood.h"
+
+@implementation FREFood
+
+@end
